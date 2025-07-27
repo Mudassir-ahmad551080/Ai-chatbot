61c629be201aa8d99d80bb739de492ab4e7e2bd8
Mudassir Ahmad <ma6386731@gmail.com>
2025-07-27 14:28:51 +0500

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,4 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//       <h1>this is the app component</h1>
-//       <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius natus nobis veniam dolorem reprehenderit officiis dolores sint pariatur itaque excepturi?</h1>
-//     </>
-//   )
-// }
-
-// export default App
-
-
+// chatbot/src/App.jsx
 import React, { useState, useEffect } from 'react';
 import Answe from './component/Answe';
 
@@ -165,7 +127,7 @@ const App = () => {
             />
             <button
               onClick={askQuestion}
-              className="w-full sm:w-auto bg-black px-5 py-2 rounded-xl hover:bg-gray-700 transition"
+              className="w-full cursor-pointer sm:w-auto bg-black px-5 py-2 rounded-xl hover:bg-gray-700 transition"
             >
               Ask
             </button>
